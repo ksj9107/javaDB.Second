28b2af5ad6e9551097137ff42a6ad2abd1588c01
Unknown <tmd9107@naver.com>
2018-08-01 17:20:02 +0900

    6회차 : Lotto 완성

--- src/Day5/lotto.java
+++ src/Day5/lotto.java
@@ -9,10 +9,22 @@ public class lotto {
 		 * */
 		/* 랜덤으로 중복되지 않는 로또 번호를 생성 */
 		int min=1,max=45,size=6;
-		int arr[];
+		int arr[]=null;
+		int arr2[]=null; // 당첨 번호
+		int checkarr[] = new int[2];
 		arr=generate(min,max,size);
+		sort(arr,size);
+		System.out.print("생성번호  ");
 		printarr(arr);
-
+		
+		arr2=generate(min,max,size+1);
+		sort(arr2,size);
+		System.out.print("당첨번호  ");
+		printarr(arr2);
+		
+		checkarr = confirm(arr,arr2);
+		System.out.println("중복횟수 : " + checkarr[0]+"\t보너스 당첨 : "+checkarr[1]);
+		rank(checkarr);
 	}
 
 	public static int randomInt(int min, int max){
@@ -40,6 +52,9 @@ public class lotto {
 		 * 2.매개변수 : int num, int arr[]
 		 * 3.리턴타입 : boolean
 		 * */
+		if(cnt > arr.length){
+			cnt = arr.length;
+		}
 		for(int i=0; i<cnt; i++){
 			if(arr[i]==num){
 				return true;
@@ -51,12 +66,25 @@ public class lotto {
 		int tmp=0,cnt=0;
 		int arr[] = new int[size];
 		if(min > max){
-			return null;
+			/*tmp = min;
+			min = max;
+			max = tmp;
+			*/return null;
 		}
 		if(max-min+1<size){
 			return null;
 		}
-		for(; cnt<arr.length;){
+		if(size < 0){
+			return null;
+		}
+/*		for(; cnt<arr.length;){
+			tmp=randomInt(min, max);
+			if(!isduplicate(tmp,arr,cnt)){
+				arr[cnt]=tmp;
+				cnt++;
+			}
+		}*/
+		while(cnt<arr.length){
 			tmp=randomInt(min, max);
 			if(!isduplicate(tmp,arr,cnt)){
 				arr[cnt]=tmp;
@@ -66,13 +94,75 @@ public class lotto {
 		return arr;
 	}
 	public static void printarr(int arr[]){
-		if(arr==null){
+/*		if(arr==null){
 			System.out.println("로또번호 생성에 실패했습니다.");
 		}else{
 			for(int i=0; i<arr.length;i++){
 				System.out.print(arr[i]+" ");
 			}
 			System.out.println();
+		}*/
+		for(int tmp:arr){
+			System.out.print(tmp+" ");
+		}
+		System.out.println();
+	}
+	public static int[] sort(int arr[],int size){
+		for(int i=0;i<size-1;i++){
+			for(int j=0; j<size-1-i; j++){
+				//앞에 있는 수가 뒤에 있는 수보다 크면 서로 교환
+				if(arr[i]<arr[j+1]){
+					int tmp = arr[i];
+					arr[i]=arr[j];
+					arr[j]=tmp;
+				}
+			}
+		}
+		return arr;
+	}
+	public static int[] confirm(int genarr[],int finarr[]){
+		int cnt = 0,bonus=0;		
+		int arr[]=new int[2];
+		/*for(int i=0; i<genarr.length;i++){
+			for(int j=0; j<genarr.length; j++){
+				if(finarr[i]==genarr[j]){
+					cnt++;
+				}
+			}
+		}*/
+		for(int i=0;i<genarr.length;i++){
+			if(isduplicate(genarr[i], finarr, genarr.length))
+				cnt++;
+		}
+		if(isduplicate(finarr[finarr.length-1], genarr, genarr.length)){
+			bonus++;
+		}
+
+		
+		arr[0]=cnt;
+		arr[1]=bonus;
+		return arr;
+	}
+	public static void rank(int arr[]){
+		switch(arr[0]){
+		case 3:
+			System.out.println("5등 입니다.");
+		case 4:
+			System.out.println("4등 당첨 입니다.");
+			break;
+		case 5:
+			if(arr[1]==1){
+				System.out.println("2등 당첨 입니다");
+			}else{
+				System.out.println("3등 당첨입니다.");
+			}
+			break;
+		case 6:
+			System.out.println("1등 당첨입니다.");
+			break;
+		default:
+			System.out.println("당첨되지 않으셨습니다.");
+			break;
 		}
 	}
 }
